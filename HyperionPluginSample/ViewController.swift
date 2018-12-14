69de90d0014d2c1794eaaf43097a2a1e2478e3bb
Kenzo Nirasawa <kenzo_nirasawa@r.recruit.co.jp>
2018-12-14 21:16:38 +0900

    Initial Commit

---
+++ HyperionPluginSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HyperionPluginSample
+//
+//  Created by Kenzo Nirasawa on 2018/12/14.
+//  Copyright © 2018年 nirazo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
